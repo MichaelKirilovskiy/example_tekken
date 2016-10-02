eea6b0f4bb63b97c3dff781db2ec15de1af3071c
Michael.Kirilovskiy <michael.kirilovskiy@gmail.com>
2016-10-02 18:32:18 +0300

    Create new project

---
+++ First_project/js/script.js
@@ -0,0 +1,68 @@
+/*!
+ * jQuery JavaScript Library v2.2.4
+ * http://jquery.com/
+ *
+ * Includes Sizzle.js
+ * http://sizzlejs.com/
+ *
+ * Copyright jQuery Foundation and other contributors
+ * Released under the MIT license
+ * http://jquery.org/license
+ *
+ * Date: 2016-05-20T17:23Z
+ */
+
+    $(document).ready(function () {
+
+        $("#back-top").hide();
+
+        $(function () {
+            $(window).scroll(function () {
+                if ($(this).scrollTop() > 100) {
+                    $('#back-top').fadeIn();
+                } else {
+                    $('#back-top').fadeOut();
+                }
+            });
+
+            $('#back-top a').click(function () {
+                $('body,html').animate({
+                    scrollTop: 0
+                }, 800);
+                return false;
+            });
+        });
+
+    });
+
+
+
+    $(document).ready(function () {
+        var nav = $('.navbar');
+
+        $(window).scroll(function () {
+            if ($(this).scrollTop() > 136) {
+                nav.addClass("f-nav");
+            }
+            else {
+                nav.removeClass("f-nav");
+            }
+        });
+    });
+
+
+(function (i, s, o, g, r, a, m) {
+    i['GoogleAnalyticsObject'] = r;
+    i[r] = i[r] || function () {
+            (i[r].q = i[r].q || []).push(arguments)
+        }, i[r].l = 1 * new Date();
+    a = s.createElement(o),
+        m = s.getElementsByTagName(o)[0];
+    a.async = 1;
+    a.src = g;
+    m.parentNode.insertBefore(a, m)
+})(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+
+ga('create', 'UA-79750496-1', 'auto');
+ga('send', 'pageview');
+
